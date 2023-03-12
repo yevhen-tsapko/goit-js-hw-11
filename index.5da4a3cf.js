5ff51494e8739789713820e908acc56409a7b373
yevhen-tsapko <111687103+yevhen-tsapko@users.noreply.github.com>
2023-03-12 15:21:50 +0000

    Deploying to gh-pages from @ yevhen-tsapko/goit-js-hw-11@f2a80d9d4501b0cf135191dc86c6712b78b1735f 🚀

--- index.5da4a3cf.js
+++
@@ -1,2 +0,0 @@
-refs={form:document.querySelector(".search-form"),searchButton:document.querySelector("button[type=submit]")},console.log(refs.searchButton);
-//# sourceMappingURL=index.5da4a3cf.js.map
--- index.6159f40f.js
+++
@@ -1,2 +0,0 @@
-refs={form:document.querySelector(".search-form"),searchButton:document.querySelector("button[type=submit]")},console.log(refs.searchButton);
-//# sourceMappingURL=index.6159f40f.js.map
