7fb6b76cc67f798251a866d7ed7499434dda2a83
Yevhen Tsapko <evgentsa@gmail.com>
2023-03-16 15:13:43 +0200

    /

--- src/fetchSeach.js
+++ src/fetchSeach.js
@@ -5,7 +5,7 @@ const key = '34336743-d2a2c454c2eb7df7235afc475';
 export default async function fetchSearch(searchWord, page) {
   return await axios
     .get(
-      `https:pixabay.com/api/?key=${key}&q=${searchWord}&image_type=photo&orientation=horizontal&safeseach=true&page=${page}&per_page=40&min_height=100`
+      `https:pixabay.com/api?key=${key}&q=${searchWord}&image_type=photo&orientation=horizontal&safeseach=true&page=${page}&per_page=40&min_height=100`
     )
     .then(response => response.data)
     .catch(error => {
