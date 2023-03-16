7c9413c3c6999f17895ee1cb54f97fbc4ff6bb7e
Yevhen Tsapko <evgentsa@gmail.com>
2023-03-16 09:45:24 +0200

    deleted "await"

--- src/fetchSeach.js
+++ src/fetchSeach.js
@@ -3,7 +3,7 @@ const key = '34336743-d2a2c454c2eb7df7235afc475';
 // let page = 1;
 
 export default async function fetchSearch(searchWord, page) {
-  return await axios
+  return axios
     .get(
       `https:pixabay.com/api/?key=${key}&q=${searchWord}&image_type=photo&orientation=horizontal&safeseach=true&page=${page}&per_page=40&min_height=100`
     )
