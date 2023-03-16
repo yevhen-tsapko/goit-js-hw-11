429f937ebfaa37e0d0a49c015b373dc71ad61c4f
Yevhen Tsapko <evgentsa@gmail.com>
2023-03-16 09:56:59 +0200

    For axios: import instead of require

--- src/fetchSeach.js
+++ src/fetchSeach.js
@@ -1,9 +1,10 @@
-const axios = require('axios/dist/browser/axios.cjs');
+// const axios = require('axios/dist/browser/axios.cjs');
+import axios from 'axios';
 const key = '34336743-d2a2c454c2eb7df7235afc475';
 // let page = 1;
 
-export default function fetchSearch(searchWord, page) {
-  return axios
+export default async function fetchSearch(searchWord, page) {
+  return await axios
     .get(
       `https:pixabay.com/api/?key=${key}&q=${searchWord}&image_type=photo&orientation=horizontal&safeseach=true&page=${page}&per_page=40&min_height=100`
     )
